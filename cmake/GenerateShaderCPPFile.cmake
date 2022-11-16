a1e441627eed55e947cb9302dc4fc32f1da93499
AmamiyaRenn <379617892@qq.com>
2022-11-16 21:58:34 +0800

    构建 glsl shader

---
+++ cmake/GenerateShaderCPPFile.cmake
@@ -0,0 +1,47 @@
+﻿# ###################################################################################################
+# This function converts any file into C/C++ source code.
+# Example:
+# - input file: data.dat
+# - output file: data.h
+# - variable name declared in output file: DATA
+# - data length: sizeof(DATA)
+# embed_resource("data.dat" "data.h" "DATA")
+# ###################################################################################################
+
+function(embed_resource resource_file_name source_file_name variable_name)
+    if(EXISTS "${source_file_name}")
+        if("${source_file_name}" IS_NEWER_THAN "${resource_file_name}") # 当已经存在source且比resource更新时不需要进行操作
+            return()
+        endif()
+    endif()
+
+    message(NOTICE embed_resource)
+
+    if(EXISTS "${resource_file_name}")
+        file(READ "${resource_file_name}" hex_content HEX) # hex读入
+
+        # 正则表达式替换，读入32个十六进制数，转换为16个0x??
+        string(REPEAT "[0-9a-f]" 32 pattern)
+        string(REGEX REPLACE "(${pattern})" "\\1\n" content "${hex_content}")
+        string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1, " content "${content}")
+        string(REGEX REPLACE ", $" "" content "${content}")
+
+        # 向量定义
+        set(array_definition "static const std::vector<unsigned char> ${variable_name} =\n{\n${content}\n};")
+
+        # 添加注释、#include与定义
+        get_filename_component(file_name ${source_file_name} NAME)
+        set(source "/**\n * @file ${file_name}\n * @brief Auto generated file.\n */\n#include <vector>\n${array_definition}\n")
+
+        # 文件写入
+        file(WRITE "${source_file_name}" "${source}")
+    else()
+        message("ERROR: ${resource_file_name} doesn't exist")
+        return()
+    endif()
+endfunction()
+
+# let's use it as a script
+if(EXISTS "${PATH}")
+    embed_resource("${PATH}" "${HEADER}" "${GLOBAL}") # 函数于此处被调用
+endif()
---
+++ cmake/ShaderCompile.cmake
@@ -0,0 +1,41 @@
+﻿function(compile_shader SHADERS TARGET_NAME SHADER_INCLUDE_FOLDER GENERATED_DIR GLSLANG_BIN)
+    set(working_dir "${CMAKE_CURRENT_SOURCE_DIR}")
+
+    set(ALL_GENERATED_SPV_FILES "")
+    set(ALL_GENERATED_CPP_FILES "")
+
+    if(UNIX) # 对于unix系统，使用命令行执行命令从而使对应应用可以执行
+        execute_process(COMMAND chmod a+x ${GLSLANG_BIN})
+    endif()
+
+    # For each shader, we create a header file
+    foreach(SHADER ${SHADERS})
+        # Prepare a header name and a global variable for this shader
+        get_filename_component(SHADER_NAME ${SHADER} NAME)
+        string(REPLACE "." "_" HEADER_NAME ${SHADER_NAME})
+        string(TOUPPER ${HEADER_NAME} GLOBAL_SHADER_VAR)
+
+        set(SPV_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${GENERATED_DIR}/spv/${SHADER_NAME}.spv")
+        set(CPP_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${GENERATED_DIR}/cpp/${HEADER_NAME}.h")
+
+        add_custom_command( # 定义构建命令：glslangValidator编译shader文件为spir-v文件到指定目录
+            OUTPUT ${SPV_FILE}
+            COMMAND ${GLSLANG_BIN} -I${SHADER_INCLUDE_FOLDER} -V100 -o ${SPV_FILE} ${SHADER} # -g -Od should only be used in debug version
+            DEPENDS ${SHADER}
+            WORKING_DIRECTORY "${working_dir}")
+
+        list(APPEND ALL_GENERATED_SPV_FILES ${SPV_FILE})
+
+        add_custom_command( # 定义构建命令：Make a C header file with the SPIR-V shader(using GenerateShaderCPPFile.cmake)
+            OUTPUT ${CPP_FILE}
+            COMMAND ${CMAKE_COMMAND} -DPATH=${SPV_FILE} -DHEADER="${CPP_FILE}"
+            -DGLOBAL="${GLOBAL_SHADER_VAR}" -P "${PICCOLO_ROOT_DIR}/cmake/GenerateShaderCPPFile.cmake"
+            DEPENDS ${SPV_FILE}
+            WORKING_DIRECTORY "${working_dir}")
+
+        list(APPEND ALL_GENERATED_CPP_FILES ${CPP_FILE})
+    endforeach()
+
+    add_custom_target(${TARGET_NAME}
+        DEPENDS ${ALL_GENERATED_SPV_FILES} ${ALL_GENERATED_CPP_FILES} SOURCES ${SHADERS})
+endfunction()
